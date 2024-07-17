1046e9b35a354d863460cb1082be0fa20bd27da6
Nelonn <42481486+Nelonn@users.noreply.github.com>
2024-07-17 12:34:23 +0300

    Block: added UseShapeForLightOcclusion

--- DataGenerator/src/main/java/net/minestom/generators/BlockGenerator.java
+++ DataGenerator/src/main/java/net/minestom/generators/BlockGenerator.java
@@ -173,6 +173,7 @@ public final class BlockGenerator extends DataGenerator {
         appendState(blockJson, state, "interactionShape", blockState.getInteractionShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO).toAabbs().toString(), String.class);
         appendState(blockJson, state, "occlusionShape", blockState.getOcclusionShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO).toAabbs().toString(), String.class);
         appendState(blockJson, state, "visualShape", blockState.getVisualShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO, CollisionContext.empty()).toAabbs().toString(), String.class);
+        appendState(blockJson, state, "UseShapeForLightOcclusion", blockState.useShapeForLightOcclusion(), boolean.class);
 
         appendState(blockJson, state, "IsRandomlyTicking", blockState.isRandomlyTicking(), boolean.class);
 
